eb5b43f866b14565d979f1cb593b41a2276a5d07
zukki259 <yuya.su@outlook.jp>
2017-02-10 21:58:58 +0900

    無限ループの原因になっていたラベル名を修正

--- lib_assem_tail.s
+++ lib_assem_tail.s
@@ -4,11 +4,11 @@ _print_char_flush:
 	lw	%r3,0(%r2)
 	lw	%r4,1(%r2)
 	addi	%r5,%r0,4
-_print_char_flush_loop:		#4バイトに足りない分の空白を詰める(今回はそれで一応問題ない
+_print_char_flush_loop_e:		#4バイトに足りない分の空白を詰める(今回はそれで一応問題ない
 	sll	%r3,%r3,8
 	ori	%r3,%r3,0x20
 	addi	%r4,%r4,1
-	bne	%r4,%r5,_print_char_flush_loop
+	bne	%r4,%r5,_print_char_flush_loop_e
 _print_char_flush_do:
 	out	%r3
 	sw	%r0,0(%r2)
